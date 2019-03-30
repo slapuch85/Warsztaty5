205bf4c6d9f4c7bbb8e767470340edd94338224c
slawekp <slapuch85@gmail.com>
2019-03-30 16:10:43 +0100

    next commit

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,16 +1,31 @@
 package pl.coderslab;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-    @RestController
-    @RequestMapping("/books")
-    public class BookController {
-        @RequestMapping("/hello")
-        public String hello(){
-            return "{hello: World}";
-        }
+@RestController
+@RequestMapping("/books")
+
+public class BookController {
+
+    private MemoryBookService memBookService;
+    @Autowired
+    public BookController(MemoryBookService memBookService) {
+        this.memBookService = memBookService;
     }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "{hello: World}";
+    }
+
+    @RequestMapping("/helloBook")
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
+}
 
