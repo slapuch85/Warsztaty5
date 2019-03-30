5573babf852f8dac6cfba3f8f235f562ac338c67
slawekp <slapuch85@gmail.com>
2019-03-30 15:48:30 +0100

    next commit

---
+++ src/main/java/pl/coderslab/Book.java
@@ -0,0 +1,12 @@
+package pl.coderslab;
+
+public class Book {
+    private long id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+}
+
+
